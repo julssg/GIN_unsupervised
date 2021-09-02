5071b3ca54666ea5dce85db710e3024f416867e4
Julia Siegl <siegl@stud.uni-heidelberg.de>
2021-09-02 11:29:38 +0200

    make permutation mapping between learned clusters, diff models

--- emnist.py
+++ emnist.py
@@ -4,75 +4,81 @@ from time import time
 import torch
 from model import GIN
 from collections import OrderedDict
-
-parser = argparse.ArgumentParser(description='Experiments on EMNIST with GIN (training script)')
-parser.add_argument('--n_epochs', type=int, default=100,
-                    help='Number of training epochs (default 100)')
-parser.add_argument('--epochs_per_line', type=int, default=1,
-                    help='Print a new line after this many epochs (default 1)')
-parser.add_argument('--lr', type=float, default=3e-4,
-                    help='Learn rate (default 3e-4)')
-parser.add_argument('--lr_schedule', nargs='+', type=int, default=[50],
-                    help='Learn rate schedule (decrease lr by factor of 10 at these epochs, default [50]). \
-                            Usage example: --lr_schedule 20 40')
-parser.add_argument('--batch_size', type=int, default=240,
-                    help='Batch size (default 240)')
-parser.add_argument('--save_frequency', type=int, default=10,
-                    help='Save a new checkpoint and make plots after this many epochs (default 10)')
-parser.add_argument('--data_root_dir', type=str, default='./',
-                    help='Directory in which \'EMNIST\' directory storing data is located (defaults to current directory). If the data is not found here you will be prompted to download it')
-parser.add_argument('--incompressible_flow', type=int, default=1,
-                    help='Use an incompressible flow (GIN) (1, default) or compressible flow (GLOW) (0)')
-parser.add_argument('--empirical_vars', type=int, default=1,
-                    help='Estimate empirical variables (means and stds) for each batch (1, default) or learn them along \
-                            with model weights (0)')
-parser.add_argument('--unsupervised', type=int, default=0,
-                    help='State whether model should be trained supervised (0, default) or unsupervised \
-                            with leared clustering in latent space (1)')
-parser.add_argument('--n_clusters', type=int, default=40,
-                    help='Number of components in gaussian mixture (default 40)')
-parser.add_argument('--n_runs', type=int, default=1,
-                    help='Number of runs (default 1)')
-
-args = parser.parse_args()
-
-assert args.incompressible_flow in [0,1], 'Argument should be 0 or 1'
-assert args.empirical_vars in [0,1], 'Argument should be 0 or 1'
-assert args.unsupervised in [0,1], 'Argument should be 0 or 1'
-
-model = GIN(dataset='EMNIST', 
-            n_epochs=args.n_epochs, 
-            epochs_per_line=args.epochs_per_line, 
-            lr=args.lr, 
-            lr_schedule=args.lr_schedule, 
-            batch_size=args.batch_size, 
-            save_frequency=args.save_frequency, 
-            data_root_dir=args.data_root_dir, 
-            incompressible_flow=args.incompressible_flow, 
-            empirical_vars=args.empirical_vars,
-            unsupervised=args.unsupervised,
-            n_classes=args.n_clusters)
+from evaluate import mcc_evaluation
 
 
-                
+def arg_parse():
+        parser = argparse.ArgumentParser(description='Experiments on EMNIST with GIN (training script)')
+        parser.add_argument('--n_epochs', type=int, default=100,
+                        help='Number of training epochs (default 100)')
+        parser.add_argument('--epochs_per_line', type=int, default=1,
+                        help='Print a new line after this many epochs (default 1)')
+        parser.add_argument('--lr', type=float, default=3e-4,
+                        help='Learn rate (default 3e-4)')
+        parser.add_argument('--lr_schedule', nargs='+', type=int, default=[50],
+                        help='Learn rate schedule (decrease lr by factor of 10 at these epochs, default [50]). \
+                                Usage example: --lr_schedule 20 40')
+        parser.add_argument('--batch_size', type=int, default=240,
+                        help='Batch size (default 240)')
+        parser.add_argument('--save_frequency', type=int, default=10,
+                        help='Save a new checkpoint and make plots after this many epochs (default 10)')
+        parser.add_argument('--data_root_dir', type=str, default='./',
+                        help='Directory in which \'EMNIST\' directory storing data is located (defaults to current directory). If the data is not found here you will be prompted to download it')
+        parser.add_argument('--incompressible_flow', type=int, default=1,
+                        help='Use an incompressible flow (GIN) (1, default) or compressible flow (GLOW) (0)')
+        parser.add_argument('--empirical_vars', type=int, default=1,
+                        help='Estimate empirical variables (means and stds) for each batch (1, default) or learn them along \
+                                with model weights (0)')
+        parser.add_argument('--unsupervised', type=int, default=0,
+                        help='State whether model should be trained supervised (0, default) or unsupervised \
+                                with leared clustering in latent space (1)')
+        parser.add_argument('--n_clusters', type=int, default=40,
+                        help='Number of components in gaussian mixture (default 40)')
+        parser.add_argument('--n_runs', type=int, default=1,
+                        help='Number of runs (default 1), if 0 selected, than only evaluation will be performed \
+                                path needs to be given in emnist.py script.')
+
+        args = parser.parse_args()
+
+        assert args.incompressible_flow in [0,1], 'Argument should be 0 or 1'
+        assert args.empirical_vars in [0,1], 'Argument should be 0 or 1'
+        assert args.unsupervised in [0,1], 'Argument should be 0 or 1'
+
+        return args
+
+
+def main():
+        args = arg_parse()
+
+        model = model_init(args)
+
+        if args.n_runs == 1:
+                model.train_model()
+        elif args.n_runs == 0:
+                save_dir = os.path.join('./emnist_save/', 'many_runs', '1630500545')
+                mcc_evaluation(args, model, save_dir)
+        else:
+                timestamp = str(int(time()))
+                save_dir = os.path.join('./emnist_save/', 'many_runs', timestamp)
+                os.makedirs(save_dir)
+
+                for run in range(args.n_runs):
+                        print(f"Starting {run+1} run:")
+                        model.train_model()
+                        save(model, os.path.join(save_dir, f'{run+1}.pt'))
+
+                        model = model_init(args)
+
+                # mcc_evaluation(args, model, save_dir)
+
+
 def save(model, fname):
         state_dict = OrderedDict((k,v) for k,v in model.state_dict().items() if not k.startswith('net.tmp_var'))
         torch.save({'model': state_dict}, fname)
 
 
-if args.n_runs == 1:
-        model.train_model() 
-else:
-        timestamp = str(int(time()))
-        save_dir = os.path.join('./emnist_save/', 'many_runs', timestamp)
-        os.makedirs(save_dir)
-
-        for run in range(args.n_runs):
-                print(f"Starting {run+1} run:")
-                model.train_model()
-                save(model, os.path.join(save_dir, f'{run+1}.pt'))
-
-                model = GIN(dataset='EMNIST', 
+def model_init(args):
+        model = GIN(dataset='EMNIST', 
                         n_epochs=args.n_epochs, 
                         epochs_per_line=args.epochs_per_line, 
                         lr=args.lr, 
@@ -84,5 +90,8 @@ else:
                         empirical_vars=args.empirical_vars,
                         unsupervised=args.unsupervised,
                         n_classes=args.n_clusters)
+        return model
 
-        # mcc( model, save_dir, args.data_root_dir)
+
+if __name__ == '__main__':
+    main()
--- evaluate.py
+++ evaluate.py
@@ -3,47 +3,78 @@ import os
 from os import listdir
 from os.path import isfile, join
 import torch
+import statistics
+from statistics import mode
 
 from data import make_dataloader_emnist
 
 
-def mcc_evaluation(model, save_dir, data_root_dir):
-    saved_models = [f for f in listdir(save_dir) if isfile(join(save_dir, f))]
+def mcc_evaluation(args, model, save_dir):
+    saved_models = [join(save_dir, f) for f in listdir(save_dir) if isfile(join(save_dir, f))]
     num_runs = len(saved_models)
 
     # 1: load test data set
 
-    test_loader  = make_dataloader_emnist(batch_size=1000, train=False, root_dir=data_root_dir)
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    test_loader  = make_dataloader_emnist(batch_size=10000, train=False, root_dir=args.data_root_dir)
+    device = 'cpu' #'cuda' if torch.cuda.is_available() else 'cpu'
 
     for i in range(num_runs):
         for j in range(num_runs):
             if j != i:
-                model_ref = torch.load(model, saved_models[i])
-                model_comp = torch.load(model, saved_models[j])
+                print(f"Using models {saved_models[i]} as reference model and {saved_models[j]} model to compare with.")
 
-                model_ref.eval().to(device)
-                model_comp.eval().to(device)
+                # model_comp =  load(model, saved_models[4], device)
 
                 for batch_idx, (data, target) in enumerate(test_loader):
                 # 2: classify 30% data with model_ref and model_comp   # only use 10000 data
                     data.to(device)
-                    if batch_idx < 3:
-                        z, logdet_J = model_ref.net(data)
-                        y_ref = model_ref.predict_y(z)
+                    if batch_idx < 1:
 
+                        model_ref = load(model, saved_models[i], device) 
+                        z, logdet_J = model_ref.net(data)
+                        y_ref = model_ref.predict_y(z.to(device))
+                        print(y_ref)
+
+                        model_comp =  load(model, saved_models[j], device)
                         z, logdet_J = model_comp.net(data)
-                        y_comp = model_comp.predict_y(z)
+                        y_comp = model_comp.predict_y(z.to(device))
                         # 3: learn linear permutation between classes between both models 
-                            # Code here
+                        # print(len(y_ref), len(y_comp))
+                        print(y_comp)
 
-                    if batch_idx < 11:
-                        z, logdet_J = model_ref.net(data)
-                        y_ref = model_ref.predict_y(z)
+                        print(y_ref - y_comp)
+                        print(sum(y_ref - y_comp))
+                        learn_permutation(y_ref, y_comp, args.n_clusters)
 
-                        z, logdet_J = model_comp.net(data)
-                        y_comp = model_comp.predict_y(z)
+
+                    # elif batch_idx < 11:
+                       # z, logdet_J = model_ref.net(data)
+                       # y_ref = model_ref.predict_y(z.to(device))
+
+                        # z, logdet_J = model_comp.net(data)
+                        # y_comp = model_comp.predict_y(z.to(device))
                 # 4: classify rest data with both models and use classification model_ref = y_true and model_comp = y_pred
                 # 5: calculate MCC
                     # Code here
+
+def load(model_init, fname, device):
+    model = model_init
+    data = torch.load(fname)
+    model.load_state_dict(data['model'])
+    model.eval()
+    model.to(device)
+    return model
+    
+def learn_permutation(y_ref, y_comp, n_clusters):
+
+    for cluster in range(n_clusters):
+        idx_of_samle_classes = [i for i, j in enumerate(y_ref) if j == cluster ]
+        # print(idx_of_samle_classes)
+        # print(len(idx_of_samle_classes))
+        if idx_of_samle_classes:
+            y_permuted = mode(y_comp[idx_of_samle_classes])
+        else:
+            y_permuted = None
+        print("Pertupation pair: ", cluster, y_permuted)
+
 
